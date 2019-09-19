3fe37c17c706b8dbbb65d1870525b77c035f0235
ruklakhani <54560558+ruklakhani@users.noreply.github.com>
2019-09-18 22:34:36 -0700

    test function added

--- spaceMan.py
+++ spaceMan.py
@@ -3,9 +3,9 @@ import random
 # Declarations
 fave_words = ["cooky", "imagine", "incredible"]
 
-# user input return
+
+# user input returns
 def user_input(prompt):
-    # validation
     try:
         i = input(prompt)
 
@@ -16,15 +16,25 @@ def user_input(prompt):
     except(err):
         print("Error: {}".format(err))
 
-# randomized word based on topic selection
+
+# random word choices
 def pickWord():
     choice = user_input("Topic? \n 1 - ruk's favorite words \n\n")
 
-    # choice
     if choice == "1":
-        # pick a random index appropriate for the given chosen index
+        # pick index
         rand = random.randint(1, len(fave_words) - 1)
         hiddenWord = fave_words[rand]
+        # tuple to return index and word
+        hiddenValues = (len(hiddenWord), hiddenWord)
+        return hiddenValues
+    else:
+        print("Invalid response- pick again \n")
+        pickWord()
+    if choice == "2":
+        # pick index
+        rand = random.randint(1, len(best_pals) - 1)
+        hiddenWord = best_pals[rand]
         # tuple to return index and word
         hiddenValues = (len(hiddenWord), hiddenWord)
         return hiddenValues
@@ -34,7 +44,6 @@ def pickWord():
 
 
 def play(word):
-
     # declarations
     hiddenWord = word[1]
     hiddenLength = word[0]
@@ -43,20 +52,18 @@ def play(word):
     blanks = ["X"]
     counter = 0
 
-    # populate blanks appropriately
+    # populate blanks
     for x in range(hiddenLength - 1):
         blanks.append("X")
 
-    # run loop and main process
+    # run loop
     while not end:
 
-        # readout
         joined = ' '.join(blanks)
         print(joined + '\n')
         print("Guesses: {}\n\n".format(lives))
         guess = user_input("Guess a letter: \n")
 
-        # correct/incorrect
         wrong = True
 
         for i, x in enumerate(hiddenWord):
@@ -73,27 +80,34 @@ def play(word):
             print("Oof pal, try again \n")
             lives = lives - 1
 
-        # end conditions
+        # the end
         if (lives == 0):
             print("Wapow! You goofed it... Try again?")
         elif (counter == hiddenLength):
             print("Oooooooh BAYBEE")
 
-        # play again
+        # take two
         if lives == 0 or counter == hiddenLength:
             again = user_input("Wanna go again? (Y/N) \n")
             again = again.lower()
 
-            try:
-                if(code == "y"):
-                    blanks = ["X"]
-                    lives = 7
-                    return False
-                if(code == "n"):
-                    return
-            except:
-                print("Invalid input")
-                return True
+        # try:
+        #     if(code == "y"):
+        #         blanks = ["X"]
+        #         lives = 7
+        #         return False
+        #     if(code == "n"):
+        #         return
+        # except:
+        #     print("Invalid input")
+        #     return True
+
+
+# This is the only function I can test :(
+def test():
+    assert(pickWord() in fave_words), "pickWord() isn't working"
+
+
 
 #initial run
 done = False
@@ -102,6 +116,7 @@ code = code.lower()
 
 if code == "n":
     done = True
+
 
 #run loop
 while not done:
